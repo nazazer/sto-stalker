78e4e1f104789cc16aec5f7877b82a2927768f03
nazazer <nazar.mikhai@gmail.com>
2022-01-21 22:36:26 +0200

    js_update

--- js/script.js
+++ js/script.js
@@ -3,10 +3,15 @@ window.onload = function () {
     window.setTimeout(function () {
       document.body.classList.add('loaded');
       document.body.classList.remove('loaded_hiding');
-    }, 1000);
-  }
+  }, 1000);
+}
+
+let emptdiv = document.createElement('div');
+emptdiv.className = "empty";
+emptdiv.onclick = function(){closeM();};
 
 function showM(){
+    document.body.append(emptdiv);
     document.getElementById("addwin").classList.add("hider");
     document.getElementById("addwin").appendChild(document.getElementById("headlist"));
     document.getElementById("headlist").classList.add("visible");
@@ -16,18 +21,27 @@ function closeM(){
     document.getElementById("addwin").classList.remove('hider');
     document.getElementById("headlist").classList.remove("visible");
     document.getElementById("wrhead").appendChild(document.getElementById("headlist"));
+    emptdiv.remove();
 }
 
 window.onscroll = function(){checkMenu()};
 
 let header = document.getElementById("headlist");
 let sticky = header.offsetTop;
+mheader = document.getElementById("menubutton");
+let msticky = mheader.offsetTop;
 
 function checkMenu(){
   var width = (window.innerWidth > 0) ? window.innerWidth : screen.width;
-  if(window.scrollY >= sticky && +width >= 900){
+  if (window.scrollY >= sticky && +width >= 900){
     header.classList.add("sticky");
-  }else{
+  } else {
     header.classList.remove("sticky");
+  }
+
+  if (window.scrollY >= msticky && +width < 900) {
+    mheader.classList.add("msticky");
+  } else {
+    mheader.classList.remove("msticky");
   }
 }
